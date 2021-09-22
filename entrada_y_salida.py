243466f716e0d31661045efed35d63038c3625b2
GAEL MONTIEL <90258888+Gaell123@users.noreply.github.com>
2021-09-22 01:48:48 +0000

    Añadi mi promedio py

---
+++ entrada_y_salida.py
@@ -0,0 +1,15 @@
+# Este programa muestra la forma de recoger informacion desde el teclado
+# 20 de septiembre
+
+print("Hola! como te llamaas?")
+miNombre = input() #input() recibe texto desde el teclado
+print("Hola", miNombre, "espero que lo estes pasando genial")
+print("Que edad tienes?")
+edad = input()
+
+if edad > 13:
+  print("Ok, puedes entrar")
+else:
+  print ("mm no, aqui no se permiten niños")
+
+
